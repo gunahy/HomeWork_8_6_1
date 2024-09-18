36b0647c767b9a6bac95a2e5b78ce25c6aa56592
gunahy <freshdv@mail.ru>
2024-09-18 21:40:03 +0500

    Добавлен класс DirectorySize переименован namespace Module8

---
+++ DirectorySize.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8
+{
+    public static class DirectorySize
+    {
+        /// <summary>
+        /// Класс DirectorySize предназначен для вычисления общего размера заданной директории, 
+        /// включая все вложенные подкаталоги и файлы.
+        /// 
+        /// Особенности:
+        /// - Если доступ к каким-либо файлам или директориям запрещен (например, отсутствуют права доступа),
+        ///   программа обрабатывает исключение и выводит предупреждающее сообщение, не прерывая выполнение.
+        /// - Класс является статическим и может использоваться для работы с любыми директориями на диске. 
+        /// Методы:
+        /// - GetDirectorySize(string folderPath): Рекурсивно вычисляет размер всех файлов в заданной директории.
+        /// 
+        /// Исключения:
+        /// - DirectoryNotFoundException: выбрасывается, если директория по указанному пути не найдена.
+        /// - UnauthorizedAccessException: выбрасывается, если доступ к файлу или папке запрещен.
+        /// </summary>
+        public static long GetDirectorySize(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                throw new DirectoryNotFoundException($"Папка {path} не найдена");
+            }
+
+            long size = 0;
+
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+                foreach (FileInfo file in dirInfo.GetFiles())
+                {
+                    size += file.Length;
+                }
+
+                foreach (DirectoryInfo subPath in dirInfo.GetDirectories())
+                {
+                    size += GetDirectorySize(subPath.FullName);
+                }
+            }
+            catch (UnauthorizedAccessException e) {
+
+                Console.WriteLine($"Ошибка: Недостаточно прав для доступа к файлам. {e.Message}");
+
+            }
+
+            return size;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,139 +1,145 @@
 ﻿using System;
 using System.IO;
 
-class Program
+
+namespace Module8
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        string directoryPath = @"C:\testProgram"; // Путь к папке
-        TimeSpan timeLimit = TimeSpan.FromMinutes(30);
-
-        // Путь к лог-файлу во временной папке
-        string logFilePath = Path.GetTempFileName();
-
-        try
+        static void Main(string[] args)
         {
-            CleanDirectory(directoryPath, timeLimit, logFilePath);
-        }
-        catch (Exception e)
-        {
-            // Логируем исключение в файл
-            LogException(logFilePath, e);
-        }
+            string directoryPath = @"C:\testProgram"; // Путь к папке
+            TimeSpan timeLimit = TimeSpan.FromMinutes(30);
 
-        Console.WriteLine("Операция завершена.");
+            // Путь к лог-файлу во временной папке
+            string logFilePath = Path.GetTempFileName();
 
-        // Спрашиваем пользователя, нужно ли открыть файл лога
-        Console.Write("Открыть лог-файл? Д/н: ");
-        string response = Console.ReadLine();
-
-        if (response?.Trim().ToLower() == "д")
-        {
             try
             {
-                System.Diagnostics.Process.Start("notepad.exe", logFilePath);
+                CleanDirectory(directoryPath, timeLimit, logFilePath);
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Не удалось открыть файл: {e.Message}");
+                // Логируем исключение в файл
+                LogException(logFilePath, e);
             }
-        }
-    }
 
-    /// <summary>
-    /// Рекурсивный метод для очистки директорий. Удаляет файлы и папки, которые не использовались дольше указанного времени.
-    /// </summary>
-    /// <param name="directoryPath">Путь к директории для очистки.</param>
-    /// <param name="timeLimit">Время, по истечении которого файлы/папки считаются устаревшими и подлежат удалению.</param>
-    /// <param name="logFilePath">Запись результата удаления файла/папки в лог-файл</param>
-    static void CleanDirectory(string directoryPath, TimeSpan timeLimit, string logFilePath)
-    {
-        // Проверяем, существует ли директория перед обработкой
-        if (!Directory.Exists(directoryPath))
-        {
-            Console.WriteLine($"Директория не существует: {directoryPath}");
-            return;
-        }
+            Console.WriteLine("Операция завершена.");
 
-        // Удаление файлов в текущей директории
-        foreach (var file in Directory.GetFiles(directoryPath))
-        {
-            DateTime lastAccessTime = File.GetLastAccessTime(file);
-            if (DateTime.Now - lastAccessTime > timeLimit)
+            // Спрашиваем пользователя, нужно ли открыть файл лога
+            Console.Write("Открыть лог-файл? Д/н: ");
+            string response = Console.ReadLine();
+
+            if (response?.Trim().ToLower() == "д")
             {
-                Logger(logFilePath, file);
-                File.Delete(file);
+                try
+                {
+                    System.Diagnostics.Process.Start("notepad.exe", logFilePath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Не удалось открыть файл: {e.Message}");
+                }
             }
         }
 
-        // Рекурсивная очистка вложенных папок
-        foreach (var dir in Directory.GetDirectories(directoryPath))
+        /// <summary>
+        /// Рекурсивный метод для очистки директорий. Удаляет файлы и папки, которые не использовались дольше указанного времени.
+        /// </summary>
+        /// <param name="directoryPath">Путь к директории для очистки.</param>
+        /// <param name="timeLimit">Время, по истечении которого файлы/папки считаются устаревшими и подлежат удалению.</param>
+        /// <param name="logFilePath">Запись результата удаления файла/папки в лог-файл</param>
+        static void CleanDirectory(string directoryPath, TimeSpan timeLimit, string logFilePath)
         {
-            // Рекурсивно проверяем файлы во вложенных папках
-            CleanDirectory(dir, timeLimit, logFilePath);
-
-            // Проверяем и удаляем пустые папки, если они не использовались дольше указанного времени
-            DateTime lastAccessTime = Directory.GetLastWriteTime(dir);
-            Console.WriteLine(lastAccessTime.ToString());
-            if (DateTime.Now - lastAccessTime > timeLimit && IsDirectoryEmpty(dir))
+            // Проверяем, существует ли директория перед обработкой
+            if (!Directory.Exists(directoryPath))
             {
-                Logger(logFilePath, dir);
-                Directory.Delete(dir, true); 
+                Console.WriteLine($"Директория не существует: {directoryPath}");
+                return;
             }
-        }
-    }
 
-    /// <summary>
-    /// Проверяет, пуста ли директория.
-    /// </summary>
-    /// <param name="path">Путь к директории.</param>
-    /// <returns>true, если директория пуста, иначе false.</returns>
-    static bool IsDirectoryEmpty(string path)
-    {
-        return Directory.GetFiles(path).Length == 0 && Directory.GetDirectories(path).Length == 0;
-    }
-
-    /// <summary>
-    /// Логирует информацию об исключении в указанный файл.
-    /// </summary>
-    /// <param name="logFilePath">Путь к файлу для логов.</param>
-    /// <param name="e">Исключение, которое нужно записать в лог.</param>
-    static void LogException(string logFilePath, Exception e)
-    {
-        try
-        {
-            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            // Удаление файлов в текущей директории
+            foreach (var file in Directory.GetFiles(directoryPath))
             {
-                writer.WriteLine("Дата и время: " + DateTime.Now);
-                writer.WriteLine("Сообщение: " + e.Message);
-                writer.WriteLine("Error: " + e.StackTrace);
-                writer.WriteLine("---------------------------------------------------");
+                DateTime lastAccessTime = File.GetLastAccessTime(file);
+                if (DateTime.Now - lastAccessTime > timeLimit)
+                {
+                    Logger(logFilePath, file);
+                    File.Delete(file);
+                }
+            }
+
+            // Рекурсивная очистка вложенных папок
+            foreach (var dir in Directory.GetDirectories(directoryPath))
+            {
+                // Рекурсивно проверяем файлы во вложенных папках
+                CleanDirectory(dir, timeLimit, logFilePath);
+
+                // Проверяем и удаляем пустые папки, если они не использовались дольше указанного времени
+                DateTime lastAccessTime = Directory.GetLastWriteTime(dir);
+                Console.WriteLine(lastAccessTime.ToString());
+                if (DateTime.Now - lastAccessTime > timeLimit && IsDirectoryEmpty(dir))
+                {
+                    Logger(logFilePath, dir);
+                    Directory.Delete(dir, true);
+                }
             }
         }
-        catch (Exception logE)
+
+        /// <summary>
+        /// Проверяет, пуста ли директория.
+        /// </summary>
+        /// <param name="path">Путь к директории.</param>
+        /// <returns>true, если директория пуста, иначе false.</returns>
+        static bool IsDirectoryEmpty(string path)
         {
-            Console.WriteLine($"Не удалось записать лог: {logE.Message}");
+            return Directory.GetFiles(path).Length == 0 && Directory.GetDirectories(path).Length == 0;
+        }
+
+        /// <summary>
+        /// Логирует информацию об исключении в указанный файл.
+        /// </summary>
+        /// <param name="logFilePath">Путь к файлу для логов.</param>
+        /// <param name="e">Исключение, которое нужно записать в лог.</param>
+        static void LogException(string logFilePath, Exception e)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                    writer.WriteLine("Дата и время: " + DateTime.Now);
+                    writer.WriteLine("Сообщение: " + e.Message);
+                    writer.WriteLine("Error: " + e.StackTrace);
+                    writer.WriteLine("---------------------------------------------------");
+                }
+            }
+            catch (Exception logE)
+            {
+                Console.WriteLine($"Не удалось записать лог: {logE.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Логирует имя удаленного объекта во временный файл.
+        /// </summary>
+        /// <param name="logFilePath">Путь к файлу для логов.</param>
+        /// <param name="FilePath">Удаленный объект</param>
+        /// 
+        static void Logger(string logFilePath, string FilePath)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                    writer.WriteLine(DateTime.Now + " - " + FilePath + " удален");
+                }
+            }
+            catch (Exception logE)
+            {
+                Console.WriteLine($"Не удалось записать лог: {logE.Message}");
+            }
         }
     }
 
-    /// <summary>
-    /// Логирует имя удаленного объекта во временный файл.
-    /// </summary>
-    /// <param name="logFilePath">Путь к файлу для логов.</param>
-    /// <param name="FilePath">Удаленный объект</param>
-    /// 
-    static void Logger(string logFilePath, string FilePath)
-    {
-        try
-        {
-            using (StreamWriter writer = new StreamWriter(logFilePath, true))
-            {
-                writer.WriteLine(DateTime.Now + " - " + FilePath + " удален");
-            }
-        }
-        catch (Exception logE)
-        {
-            Console.WriteLine($"Не удалось записать лог: {logE.Message}");
-        }
-    }
 }
